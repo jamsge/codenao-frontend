4c00a4ec7ae68ec623a27f11a658e92ac2392847
Samanyu24X <35706393+Samanyu24X@users.noreply.github.com>
2021-11-13 23:08:00 -0800

    Added dragbar

--- index.js
+++ index.js
@@ -29,6 +29,24 @@ function changeTheme(newTheme) {
   myCodeMirror.setOption("theme", theme)
 }
 
+// related to dragbar
+var left = document.getElementById('input');
+var right = document.getElementById('drag-right');
+var bar = document.getElementById('dragbar');
+
+const drag = (e) => {
+  document.selection ? document.selection.empty() : window.getSelection().removeAllRanges();
+  left.style.width = (e.pageX - bar.offsetWidth / 2) + 'px';
+}
+
+bar.addEventListener('mousedown', () => {
+  document.addEventListener('mousemove', drag);
+});
+
+bar.addEventListener('mouseup', () => {
+  document.removeEventListener('mousemove', drag);
+});
+
 //saves on site unload
 window.addEventListener(
   "beforeunload",
